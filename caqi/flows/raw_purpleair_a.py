4180c54d5cbfc3c3509746adbaef873c19dc6d5b
Chris Gervang <chris@gervang.com>
2020-11-23 00:03:23 -0800

    Adding two varients of the same flow
    "A" uses more control flow tasks then "B". Visually "A" lets you see if an online or offline data source was queried, while "B" is a simpler flow that assumes everything follows the same structural interface.

---
+++ caqi/flows/raw_purpleair_a.py
@@ -0,0 +1,38 @@
+from caqi.daos.all_sensors_raw import AllSensorsRawDao
+from caqi.clients.purpleair_client import PurpleAirHttpClient, PurpleAirFileSystemClient
+import prefect
+from prefect import task, Flow, Parameter
+from prefect.tasks.control_flow import ifelse, merge
+
+logger = prefect.context.get("logger")
+
+@task
+def extract_offline_live_purpleair():
+    client = PurpleAirFileSystemClient()
+    all_sensors = AllSensorsRawDao(purpleair_client=client)    
+    logger.info(f"PurpleAir JSON Version: {all_sensors.get_version()}")
+    return all_sensors
+
+@task
+def extract_online_live_purpleair():
+    client = PurpleAirHttpClient()
+    all_sensors = AllSensorsRawDao(purpleair_client=client)    
+    logger.info(f"PurpleAir JSON Version: {all_sensors.get_version()}")
+    return all_sensors
+
+def main():
+    with Flow("manual-raw-purpleair-a") as flow:
+        offline = Parameter("offline", default=True)
+        all_sensors_online = extract_online_live_purpleair()
+        all_sensors_offline = extract_offline_live_purpleair()
+        ifelse(offline, all_sensors_offline, all_sensors_online)
+        all_sensors = merge(all_sensors_offline, all_sensors_online)
+
+    # Registers flow to server, which we can then deploy and run in background agents.
+    flow.register(project_name="caqi-flows")
+
+    # Immediately executes without agents
+    flow.run()
+
+if __name__ == "__main__":
+    main()
---
+++ caqi/flows/raw_purpleair_b.py
@@ -0,0 +1,67 @@
+from caqi.daos.all_sensors_processed import AllSensorsProcessedDao
+from datetime import datetime, timedelta
+import prefect
+from prefect.schedules import IntervalSchedule
+from prefect import task, Flow, Parameter
+from caqi.clients.file_system_client import FileSystemClient
+from caqi.daos.all_sensors_raw import AllSensorsRawDao
+from caqi.clients.purpleair_client import PurpleAirClient, PurpleAirHttpClient, PurpleAirFileSystemClient
+
+logger = prefect.context.get("logger")
+
+@task
+def create_purpleair_client(offline) -> PurpleAirClient:
+    if offline:
+        return PurpleAirFileSystemClient()
+    return PurpleAirHttpClient()
+
+@task
+def extract_live_purpleair(purpleair_client):
+    all_sensors_raw = AllSensorsRawDao(purpleair_client=purpleair_client)    
+    logger.info(f"PurpleAir JSON Version: {all_sensors_raw.get_version()}")
+    return all_sensors_raw
+
+@task
+def transform_all_sensors_raw(all_sensors_raw):
+    all_sensors_processed = AllSensorsProcessedDao(all_sensors_raw=all_sensors_raw)
+    return all_sensors_processed
+
+@task
+def create_hour_blob_client():
+    dt = datetime.utcnow()
+    return FileSystemClient(sub_path=f"all_sensors/year={dt.year}/month={dt.month}/day={dt.day}/hour={dt.hour}")
+
+@task
+def load_all_sensors_raw_json(all_sensors_raw, blob_client):
+    all_sensors_raw_json = all_sensors_raw.get_json()
+    blob_client.save_json(all_sensors_raw_json, "raw")
+
+@task
+def load_all_sensors_processed(all_sensors_processed, blob_client):
+    all_sensors_processed_df = all_sensors_processed.get_raw_df()
+    blob_client.save_csv(all_sensors_processed_df, "processed")
+
+def main():
+
+    schedule = IntervalSchedule(
+        start_date=datetime.utcnow() + timedelta(seconds=1), 
+        interval=timedelta(hours=1)
+    )
+
+    with Flow("manual-raw-purpleair-b", schedule=schedule) as flow:
+        offline = Parameter("offline", default=False)
+        purpleair_client = create_purpleair_client(offline)
+        all_sensors_raw = extract_live_purpleair(purpleair_client)
+        all_sensors_processed = transform_all_sensors_raw(all_sensors_raw)
+        blob_client = create_hour_blob_client()
+        load_all_sensors_raw_json(all_sensors_raw, blob_client)
+        load_all_sensors_processed(all_sensors_processed, blob_client)
+
+    # Registers flow to server, which we can then deploy and run in background agents.
+    flow.register(project_name="caqi-flows")
+
+    # Immediately executes without agents
+    # flow.run()
+
+if __name__ == "__main__":
+    main()
